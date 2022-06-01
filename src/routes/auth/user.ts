a1bbea77fc49a08ca4ea247ae10ab82423207f10
Sooseki <bonnetmanon3006@gmail.com>
2022-06-01 12:46:54 +0200

    init commit

---
+++ src/routes/auth/user.ts
@@ -0,0 +1,15 @@
+import { CrudOperations, CrudRouter } from '../../classes/CrudRouter';
+import { IUserCreate, IUserRO, IUserUpdate } from '../../types/tables/user/IUser';
+import { UserCreateValidator, UserUpdateValidator } from '../../types/tables/user/user.validator';
+
+
+export const ROUTES_USER = CrudRouter<IUserRO, IUserCreate, IUserUpdate>({
+  table: 'user',
+  primaryKey: 'userId',
+  operations: CrudOperations.Index | CrudOperations.Create | CrudOperations.Read | CrudOperations.Update | CrudOperations.Delete,
+  readColumns: ['userId', 'familyName', 'givenName', 'email'],
+  validators: {
+    create: UserCreateValidator,
+    update: UserUpdateValidator
+  }
+});
