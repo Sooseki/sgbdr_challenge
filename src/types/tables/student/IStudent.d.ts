484a27ccf5bb11947cee98977a9dbb5ee6b5db15
Robin <robinpinondev@gmail.com>
2022-06-22 17:43:50 +0200

    last fix

--- src/types/tables/student/IStudent.d.ts
+++ src/types/tables/student/IStudent.d.ts
@@ -13,11 +13,7 @@ export interface IStudent {
   /** Adress e-mail, ceci doit être unique est sera utilisé comme identifiant pour l'utilisateur */
   email_student: string;
 
-<<<<<<< HEAD
   id_prom?: number;
-=======
-  id_prom: number ;
->>>>>>> 2235ffb9a4630e35d33a7ffba7f1e50407455051
 }
 
 export type IStudentCreate = Omit<IStudent, 'student'>;
